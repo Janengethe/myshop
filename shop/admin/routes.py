ed9d744041ceae2397ccfc16307ef43413dc0f7f
Janengethe <janengethew@gmail.com>
2021-09-23 15:55:01 +0300

    second

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,11 +1,20 @@
-from flask import render_template, session, request, redirect, url_for
+from flask import render_template, session, request, redirect, url_for, flash
 
 from shop import app, db
+
+from .forms import RegistrationForm
 
 @app.route('/')
 def home():
     return 'Home page of my shop'
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('admin/register.html', title="Register user")
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        #user = User(form.username.data, form.email.data,
+        #            form.password.data)
+        #db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('admin/register.html', form=form, title="Registeration page")
